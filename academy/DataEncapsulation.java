9f3ddc5ff22c1fb653c121731e3a045f95ff117b
Mihir9702 <mkpatel9702@gmail.com>
2024-05-30 14:17:27 -0400

    organize project file structure

---
+++ academy/DataEncapsulation.java
@@ -0,0 +1,48 @@
+package academy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class DataEncapsulationCompany {
+  private String name;
+  private List<String> employees = new ArrayList<>();
+
+  public void printSorted() {
+    System.out.println("company name = " + name);
+    Collections.sort(employees);
+    System.out.println("sorted = " + employees);
+  }
+
+  public void setName(String name) {
+    if (name == null || name.isEmpty()) { throw new NullPointerException("name cannot be null"); }
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void addEmployee(String name) {
+    if (name == null || name.isEmpty()) { throw new NullPointerException("name cannot be null"); }
+    employees.add(name);
+  }
+}
+
+public class DataEncapsulation {
+  
+  public static void main(String[] args) {
+    DataEncapsulationCompany company = new DataEncapsulationCompany();
+    company.setName("MyCompany");
+    company.addEmployee("John");
+    company.addEmployee("Anthony");
+    company.addEmployee("Jimmy");
+    company.printSorted();
+
+
+    // company.addEmployee(null);
+    // company.addEmployee("Jimmy"); // NullPointerException
+ 
+    company.printSorted(); // NullPointerException because Collections.sort(null) throws NullPointerException
+  }
+}
---
+++ academy/NumberOfSections.java
@@ -0,0 +1,18 @@
+package academy;
+
+interface HasExoskeleton {
+  abstract int getNumberOfSections();
+}
+
+abstract class Insect implements HasExoskeleton {
+  abstract int getNumberOfLegs();
+}
+
+abstract class Beetle extends Insect { 
+  int getNumberOfLegs() { return 6; }
+  public abstract int getNumberOfSections();
+}
+
+public class NumberOfSections {
+  
+}
